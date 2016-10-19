99fb209e746cfd463281914140f646a1a9537d77
Phan Đức Việt <viet@techmaster.vn>
2016-10-19 18:33:23 +0700

    First commit

---
+++ src/main/java/com/chotoxautinh/Main.java
@@ -0,0 +1,89 @@
+package com.chotoxautinh;
+
+import java.io.IOException;
+
+import com.chotoxautinh.controller.RootController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class Main extends Application {
+
+	private BorderPane rootLayout;
+	private Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Test");
+		this.primaryStage.setResizable(false);
+		
+		initRootLayout();
+		
+		showStartView();
+	}
+
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// Give the controller access to the main app.
+	        RootController controller = loader.getController();
+	        controller.setMainApp(this);
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			scene.getStylesheets().add(getClass().getResource("/style/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+     * Shows the person overview inside the root layout.
+     */
+    public void showStartView() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/view/BeginLayout.fxml"));
+            VBox startView = (VBox) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(startView);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Close window
+     */
+    public void closeStage(){
+    	this.primaryStage.close();
+    }
+	
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/com/chotoxautinh/controller/RootController.java
@@ -0,0 +1,94 @@
+package com.chotoxautinh.controller;
+
+import com.chotoxautinh.Main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class RootController {
+
+	private Main mainApp;
+
+	@FXML
+	private Button homeBtn;
+
+	@FXML
+	private Button toggleMenuBtn;
+
+	@FXML
+	private Button questionBtn;
+
+	@FXML
+	private Button exitBtn;
+	
+	@FXML
+	private ListView<String> listMenu;
+
+	@FXML
+	private void initialize() {
+		loadBtnIcon();
+	}
+
+	private void loadBtnIcon() {
+		Image imageHome = new Image(getClass().getResourceAsStream("/img/home.png"));
+		homeBtn.setGraphic(new ImageView(imageHome));
+
+		Image imageToggle = new Image(getClass().getResourceAsStream("/img/menu-toggle.png"));
+		toggleMenuBtn.setGraphic(new ImageView(imageToggle));
+
+		Image imageQuestion = new Image(getClass().getResourceAsStream("/img/question.png"));
+		questionBtn.setGraphic(new ImageView(imageQuestion));
+
+		Image imageExit = new Image(getClass().getResourceAsStream("/img/exit.png"));
+		exitBtn.setGraphic(new ImageView(imageExit));
+	}
+
+	@FXML
+	private void handleExpandAction(ActionEvent event) {
+		if(listMenu.getStyleClass().indexOf("expandedMenuList") == -1){
+			listMenu.getStyleClass().add("expandedMenuList");
+		} else {
+			listMenu.getStyleClass().remove("expandedMenuList");
+		}
+	}
+	
+	@FXML
+	private void handleHomeAction(ActionEvent event) {
+		mainApp.showStartView();
+	}
+
+	@FXML
+	private void handleAboutAction(ActionEvent event) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("ChoToXauTinh - © 2016");
+		alert.setHeaderText("Contact me at:");
+		StringBuilder builder = new StringBuilder();
+		builder.append("• Class: Advanced IT 2013 - HUST\n");
+		builder.append("• Company: Techmaster\n");
+		builder.append("• Phone: 0985797649\n");
+		builder.append("• Email: viet@techmaster.vn\n");
+		alert.setContentText(builder.toString());
+
+		alert.showAndWait();
+	}
+
+	@FXML
+	private void handleExitAction(ActionEvent event) {
+		mainApp.closeStage();
+	}
+
+	public Main getMainApp() {
+		return mainApp;
+	}
+
+	public void setMainApp(Main mainApp) {
+		this.mainApp = mainApp;
+	}
+
+}
